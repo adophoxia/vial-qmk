843a1f61646306545a506ac0c3c329233a38c7bd
Eliseo Martínez <eliseomarmol@gmail.com>
2024-03-14 13:21:14 +0100

    Add support for keychron/q4_pro (ansi & iso)

---
+++ keyboards/keychron/q4_pro/ansi/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDB, 0x56, 0x52, 0x8E, 0xC9, 0x48, 0x86, 0xCA}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
