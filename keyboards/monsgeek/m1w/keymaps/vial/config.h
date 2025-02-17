488431fc75c6a86ac2c61719b602181437ddc650
Adophoxia <andyao1528@gmail.com>
2025-02-16 19:03:55 -0800

    Port M1W to Vial

---
+++ keyboards/monsgeek/m1w/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x74, 0x7A, 0x09, 0x93, 0x83, 0x2C, 0x0B, 0x05}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
